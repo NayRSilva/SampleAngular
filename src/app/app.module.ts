9840a44f003b845a0f5bccc1c3b8cb9027f4fcde
Nayara Rossi Brito da Silva <nayara.silva@eldorado.org.br>
2022-01-20 16:03:36 -0300

    Starting the project with a header

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListComponent } from './shopping-list/list/list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    RecipeListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
